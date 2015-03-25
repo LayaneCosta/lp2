d7ad63b4e0fd662aa9a2ed130d3e67bc7e7c3ce2
LayaneCosta <layane_bela@hotmail.com>
2015-03-24 21:12:26 -0300

    Create Exponencial.java
    Em construção, passando pela fase de teste. O primeiro faz tudo separado e o segundo com uma estrutura de repetição.

---
+++ exercicio2/Exponencial.java
@@ -0,0 +1,84 @@
+public class Exponencial {
+  
+  public static void main (String [] args){
+    int Q, C;
+    
+    Q = 0*0;
+    C = 0*0*0;
+    
+    System.out.printls("numero\tquadrado\tcubo");
+    System.out.printf ("0\t" + Q);
+    System.out.println("\t\t" +C);
+    
+    Q = 1*1;
+    C = 1*1*1;
+    
+    System.out.printf ("1\t" + Q);
+    System.out.println("\t\t" +C);
+    
+    Q = 2*2;
+    C = 2*2*2;
+    
+    System.out.printf ("2\t" + Q);
+    System.out.println("\t\t" +C);
+    
+    Q = 3*3;
+    C = 3*3*3;
+    
+    System.out.printf ("3\t" + Q);
+    System.out.println("\t\t" +C);
+    
+    Q = 4*4;
+    C = 4*4*4;
+    
+    System.out.printf ("4\t" + Q);
+    System.out.println("\t\t" +C);
+    
+    Q = 5*5;
+    C = 5*5*5;
+    
+    System.out.printf ("5\t" + Q);
+    System.out.println("\t\t" +C);
+    
+    Q = 6*6;
+    C = 6*6*6;
+    
+    System.out.printf ("6\t" + Q);
+    System.out.println("\t\t" +C);
+    
+    Q = 7*7;
+    C = 7*7*7;
+    
+    System.out.printf ("7\t" + Q);
+    System.out.println("\t\t" +C);
+    
+    Q = 8*8;
+    C = 8*8*8;
+    
+    System.out.printf ("8\t" + Q);
+    System.out.println("\t\t" +C);
+    
+    Q = 9*9;
+    C = 9*9*9;
+    
+    System.out.printf ("9\t" + Q);
+    System.out.println("\t\t" +C);
+    
+  }
+}
+
+public class Exponencial{
+
+  public static void main (String [] agrs){
+    System.out.println("numero\tquadrado\tcubo");
+    for (int i = 0; i <= 10; i++){
+      System.out.printf("%d\t%d\t%d%n", i, i*i, i*i*i);
+    }
+
+
+  }
+}
+
+
+
+
