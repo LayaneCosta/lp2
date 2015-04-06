a622a1147b10a9021ca7c3f84ed016879d725dd8
LayaneCosta <layane_bela@hotmail.com>
2015-04-06 17:37:14 -0300

    Update NumeroMuitoGrande.java
    Tentando fazer a multiplicação

--- exercicio3/NumeroMuitoGrande.java
+++ exercicio3/NumeroMuitoGrande.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 
 public class NumeroMuitoGrande {
@@ -9,6 +8,7 @@ public class NumeroMuitoGrande {
         String n2;
         int i, j, k;
         int r[] = new int[30];
+        int r2[] = new int[30];
         int x[] = new int[30];
         int y[] = new int[30];
         System.out.print("Primeiro Número Grande: ");
@@ -16,15 +16,15 @@ public class NumeroMuitoGrande {
         System.out.print("Segundo Número Grande: ");
         n2 = input.next();
 
-        for (i = n1.length(), j=0; i >0; i--, j++) {
+        for (i = n1.length(), j = 0; i > 0; i--, j++) {
             char caracter = n1.charAt(j);
             x[i] = Character.getNumericValue(caracter);
         }
-        for (i = n2.length(), j=0; i > 0; i--, j++) {
+        for (i = n2.length(), j = 0; i > 0; i--, j++) {
             char caracter = n2.charAt(j);
             y[i] = Character.getNumericValue(caracter);
         }
-      
+
         for (i = 29; i >= 0; i--) {
             r[i] = (x[i] + y[i]);
             if (r[i] > 9) {
@@ -32,12 +32,26 @@ public class NumeroMuitoGrande {
                 r[i] -= 10;
             }
         }
-        for (k = 29; k >= 0; k--){
-            if (r[k]>0)
-            System.out.print(r[k]);
-        
-        
+        System.out.print("Soma: ");        
+        for (k = 29; k >= 0; k--) {
+            if (r[k] > 0) {
+                System.out.print(r[k]);
+            }
         }
+        /*for (i = n1.length(); i >= n2.length(); i--) {
+            r2[i] += (x[i] + y[i]);
+            if (r2[i] > 9) {
+                r2[i - 1] = (r2[i] + 1);
+                r2[i] -= 10;
+                    System.out.print("Soma: "); 
+            }
+        }
+        System.out.println("Multplicação: ");
+        for (k = 29; k >= 0; k--) {
+            if (r2[k] > 0) {
+                System.out.print(r2[k]);
+            }
+        }*/
 
     }
 
